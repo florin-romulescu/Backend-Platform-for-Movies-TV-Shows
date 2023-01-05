3df78077a7ba47fb4a77b00f4ee1b75655c954f5
Romulescu Florin <romulescu.florin.dev@gmail.com>
2023-01-05 17:26:58 +0200

    Am adaugat optiunea de stergere film

--- src/features/DatabaseDeleteFeature.java
+++ src/features/DatabaseDeleteFeature.java
@@ -1,2 +1,55 @@
-package features;public class DatabaseDeleteFeature {
+package features;
+
+import database.Database;
+import features.strategy.FeatureStrategy;
+import fileio.ActionInput;
+import fileio.MovieInput;
+import fileio.Notification;
+import fileio.UserInput;
+import filesystem.FSConstants;
+
+public class DatabaseDeleteFeature  implements FeatureStrategy {
+    private final ActionInput action;
+
+    public DatabaseDeleteFeature(ActionInput action) {
+        this.action = action;
+    }
+
+    private boolean delete() {
+        Database instance = Database.getInstance();
+        MovieInput movie = instance.getMovie(action.getDeletedMovie());
+
+        if (movie == null) {
+            return false;
+        }
+
+        Notification msg = new Notification(movie.getName(), "DELETE");
+        instance.newMovieNotifier(msg, movie);
+
+        for (UserInput userInput: instance.getUsers()) {
+            if (userInput.getPurchasedMovies().contains(movie)) {
+                if (userInput.getCredentials().getAccountType().equals("premium")) {
+                    userInput.setNumFreePremiumMovies(
+                            userInput.getNumFreePremiumMovies() + 1
+                    );
+                } else {
+                    userInput.setTokensCount(
+                            userInput.getTokensCount() + 2
+                    );
+                }
+            }
+            userInput.getLikedMovies().remove(movie);
+            userInput.getRatedMovies().remove(movie);
+            userInput.getWatchedMovies().remove(movie);
+            userInput.getPurchasedMovies().remove(movie);
+        }
+        instance.getMovies().remove(movie);
+
+        return true;
+    }
+
+    @Override
+    public boolean action() {
+        return delete();
+    }
 }
