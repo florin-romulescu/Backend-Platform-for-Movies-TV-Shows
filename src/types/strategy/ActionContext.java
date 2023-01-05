67c771d37b93cb60acf6908ee4041dcb3cab69a7
Romulescu Florin <romulescu.florin.dev@gmail.com>
2023-01-05 17:29:30 +0200

    Am mutat optiunea de subscribe

--- src/types/strategy/ActionContext.java
+++ src/types/strategy/ActionContext.java
@@ -1,22 +1,25 @@
 package types.strategy;
 
+import features.SubscribeFeature;
 import fileio.ActionInput;
 import fileio.MovieInput;
 import fileio.UserInput;
 import filesystem.FSConstants;
-import types.ChangePageType;
-import types.OnPageType;
+import types.*;
 
 import java.util.List;
 
 public class ActionContext {
     private TypeStrategy strategy;
-    private List<MovieInput> currentMovieList;
-    private List<UserInput> users;
-    private List<MovieInput> movies;
-    private ActionInput action;
+    private final List<MovieInput> currentMovieList;
+    private final List<UserInput> users;
+    private final List<MovieInput> movies;
+    private final ActionInput action;
 
-    public ActionContext(List<MovieInput> currentMovieList, List<UserInput> users, List<MovieInput> movies, ActionInput action) {
+    public ActionContext(final List<MovieInput> currentMovieList,
+                         final List<UserInput> users,
+                         final List<MovieInput> movies,
+                         final ActionInput action) {
         this.currentMovieList = currentMovieList;
         this.users = users;
         this.movies = movies;
@@ -27,7 +30,9 @@ public class ActionContext {
         strategy = switch (action.getType()) {
             case FSConstants.CHANGE_PAGE -> new ChangePageType(action);
             case FSConstants.ON_PAGE -> new OnPageType(currentMovieList, users, movies, action);
-            case FSConstants.GO_BACK -> null;
+            case FSConstants.GO_BACK -> new GoBackType();
+            case FSConstants.DATABASE -> new DatabaseType(action);
+            case FSConstants.RECOMMENDATION -> new RecommendationType(action);
             default -> null;
         };
     }
