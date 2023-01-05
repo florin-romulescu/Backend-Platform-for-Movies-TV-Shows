4cbc0139408745f9425e2349b63ab0c594d11e4b
Romulescu Florin <romulescu.florin.dev@gmail.com>
2023-01-05 17:34:18 +0200

    Am adaugat o clasa care identifica functionalitatea de recomandare

--- src/types/RecommendationType.java
+++ src/types/RecommendationType.java
@@ -1,2 +1,23 @@
-package types;public class RecomandationType {
+package types;
+
+import features.strategy.FeatureBuilder;
+import features.strategy.FeatureContext;
+import fileio.ActionInput;
+import types.strategy.TypeStrategy;
+
+public class RecommendationType implements TypeStrategy {
+    private final ActionInput action;
+
+    public RecommendationType(ActionInput action) {
+        this.action = action;
+    }
+
+    @Override
+    public boolean action() {
+        FeatureContext featureContext = new FeatureBuilder()
+                .action(action)
+                .build();
+        featureContext.createStrategy();
+        return featureContext.action();
+    }
 }
